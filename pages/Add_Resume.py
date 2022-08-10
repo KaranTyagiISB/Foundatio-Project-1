941ffec66c8461e929e9838b023fce99986b441f
KaranTyagiISB <110120991+KaranTyagiISB@users.noreply.github.com>
2022-08-10 23:32:47 +0530

    Update Add_Resume.py

--- pages/Add_Resume.py
+++ pages/Add_Resume.py
@@ -1,5 +1,6 @@
+from pyresparser import ResumeParser
 import os
-#from docx import Document
+from docx import Document
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,18 +8,15 @@ import warnings
 warnings.filterwarnings('ignore')
 import re
 import spacy
-#from pyresparser import ResumeParser
+from pyresparser import ResumeParser
 import nltk
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-from nltk import sent_tokenize
-nltk.download('punkt')
-nltk.download('popular')
 import pandas as pd
 import numpy as np
 import sqlite3,csv
 from sqlite3 import Error
 import docx2txt
 import pdfplumber
+import psycopg2
 
 
 pd.set_option('display.max_colwidth', None)
@@ -37,6 +35,20 @@ st.text("")
 st.text("")
 
 
+with st.form("my_form"):
+    Category = st.selectbox(
+    'Please select the catgory',
+    ('None','Data Science','HR','Advocate','Arts','Web Designing','Mechanical Engineer','Sales','Health and fitness','Civil Engineer',
+    'Java Developer','Business Analyst','SAP Developer','Automation Testing','Electrical Engineering','Operations Manager'
+    'Python Developer','DevOps Engineer','Network Security Engineer','PMO','Database','Hadoop','ETL Developer','DotNet Developer'
+    'Blockchain','Testing'))
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("This candidate is a : ", Category)
+
+# st.write("Outside the form", Category)
+
 
 #elif choice == "DocumentFiles":
 # st.subheader("Document Files")
@@ -44,11 +56,6 @@ docx_file = st.file_uploader("Upload Document", type=["pdf","docx","txt"])
 
 if st.button("Process"):
     if docx_file is not None:
-
-        # file_details = {"filename":docx_file.name, "filetype":docx_file.type,
-        #                 "filesize":docx_file.size}
-        # st.write(file_details)
-
         if docx_file.type == "text/plain":
             # Read as string (decode bytes to string)
             raw_text = str(docx_file.read(),"utf-8")
@@ -67,12 +74,13 @@ if st.button("Process"):
 
 
 
-        df = pd.DataFrame(columns=['Category','Resume','Clean_Resume','Skills','Phone_Number','Email_id'])
+        df = pd.DataFrame(columns=['Category','Resume','Clean_Resume','Qualification','City','Skills','Phone_Number','Email_id'])
         df['Resume'] = [raw_text]
         Resume = str(df['Resume'][0])
 
 
         def clean_text(text) :
+            clean_text = re.sub("[\n,\r,\n\n,â¢,,\t]","",text)
             clean_text = re.sub("\\n"," ",text)
             clean_text = re.sub("\\r"," ",clean_text)
             clean_text = re.sub("â ¢"," ",clean_text)
@@ -89,17 +97,57 @@ if st.button("Process"):
         Clean_Resume = str(df["Clean_Resume"][0])
 
 
+        # For extracting Qualification
+        df_qual = pd.read_fwf('list_of_degree.txt',header=None)
+        df_qual = df_qual[0].str.replace(',','')
+        df_qual = pd.DataFrame(df_qual)
+        QUAL_DB = df_qual[0].tolist()
+
+        def extract_qual(text):
+            stop_words = set(nltk.corpus.stopwords.words('english'))
+            word_tokens = nltk.tokenize.word_tokenize(text)
+            filtered_tokens = [w for w in word_tokens if w not in stop_words]
+            filtered_tokens = [w for w in word_tokens if w.isalpha()]
+            bigrams_trigrams = list(map(' '.join, nltk.everygrams(filtered_tokens,1, 2)))
+            found_qual = set()
+            for token in filtered_tokens:
+                if token.lower() in QUAL_DB:
+                    found_qual.add(token)
+            return found_qual
+        df['Qualification']=df['Clean_Resume'].apply(lambda x: extract_qual(x))
+        Qualification = str(df["Qualification"][0])
+
+
+        # For extracting cities
+        df_city = pd.read_fwf('list_of_cities.txt',header=None)
+        df_city = df_city[0].str.replace(',','')
+        df_city = pd.DataFrame(df_city)
+        CITY_DB = df_city[0].tolist()
+        # For extracting city
+
+        def extract_city(text):
+            stop_words = set(nltk.corpus.stopwords.words('english'))
+            word_tokens = nltk.tokenize.word_tokenize(text)
+            filtered_tokens = [w for w in word_tokens if w not in stop_words]
+            filtered_tokens = [w for w in word_tokens if w.isalpha()]
+            bigrams_trigrams = list(map(' '.join, nltk.everygrams(filtered_tokens,1, 2)))
+            found_city = set()
+            for token in filtered_tokens:
+                if token.lower() in CITY_DB:
+                    found_city.add(token)
+            return found_city
+
+        df['City']=df['Clean_Resume'].apply(lambda x: extract_city(x))
+        City = str(df["City"][0])
+
         # For extracting skills
         df_skill = pd.read_fwf('list_of_skills.txt',header=None)
         df_skill = df_skill[0].str.replace(',','')
         df_skill = pd.DataFrame(df_skill)
         SKILLS_DB = df_skill[0].tolist()
-        
-        
-        
+
         def extract_skills(text):
-            #stop_words = set(nltk.corpus.stopwords.words('english'))
-            stop_words = nltk.corpus.stopwords.words('english')
+            stop_words = set(nltk.corpus.stopwords.words('english'))
             word_tokens = nltk.tokenize.word_tokenize(text)
             # remove the stop words
             filtered_tokens = [w for w in word_tokens if w not in stop_words]
@@ -119,8 +167,8 @@ if st.button("Process"):
                     found_skills.add(ngram)
 
             return found_skills
-        #df['Skills']=df['Clean_Resume'].apply(lambda x: extract_skills(x))
-        #Skills = str(df['Skills'][0])
+        df['Skills']=df['Clean_Resume'].apply(lambda x: extract_skills(x))
+        Skills = str(df['Skills'][0])
 
         # For extracting phone number from resume
         def find_phone_number(text):
@@ -137,15 +185,22 @@ if st.button("Process"):
         df['Email_id']=df['Resume'].apply(lambda x: find_email(x))
         Email_id = str(df['Email_id'][0])
 
+
         #For insering a new resume in the database
         def run_query():
-            conn = sqlite3.connect("C:/Users/ktyagi/Desktop/ISB/FP1/Project 3/resume_database.db")
+
+            conn = psycopg2.connect(database="bestfynd",
+                        host="ec2-3-6-116-139.ap-south-1.compute.amazonaws.com",
+                        user="postgres",
+                        password="mynewpassword",
+                        port="5432")
             c = conn.cursor()
-            # c.execute("INSERT INTO Resume_Table ('Category','Resume','Clean_Resume','Skills','Phone_Number','Email_id') VALUES (?,?,?,?,?,?)",('cat1',Resume,Clean_Resume,Skills,Phone_Number,Email_id))
+            c.execute("INSERT INTO Resume_Table ('Category','Resume','Clean_Resume','Qualification','City','Skills','Phone_Number','Email_id') VALUES (?,?,?,?,?,?,?,?)",(Category,Resume,Clean_Resume,Qualification,City,Skills,Phone_Number,Email_id))
             conn.commit()
             conn.close()
             st.success('Resume uploaded successfully.')
             return()
 
-        if st.button('Update',on_click = run_query):
+
+        if st.button('Upload Resume',on_click = run_query):
             run_query()
