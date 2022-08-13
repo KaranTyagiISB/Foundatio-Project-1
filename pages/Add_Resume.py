3d3bfb8a1599739d8cf58dfea0b76792a25f50ed
KaranTyagiISB <110120991+KaranTyagiISB@users.noreply.github.com>
2022-08-13 14:01:32 +0530

    Update Add_Resume.py

--- pages/Add_Resume.py
+++ pages/Add_Resume.py
@@ -194,7 +194,10 @@ if st.button("Process"):
                         password="mynewpassword",
                         port="5432")
             c = conn.cursor()
-            c.execute("INSERT INTO resume_table ('Category','Resume','Clean_Resume','Qualification','City','Skills','Phone_Number','Email_id') VALUES (?,?,?,?,?,?,?,?)",(Category,Resume,Clean_Resume,Qualification,City,Skills,Phone_Number,Email_id))
+            #c.execute("INSERT INTO resume_table ('Category','Resume','Clean_Resume','Qualification','City','Skills','Phone_Number','Email_id') VALUES (?,?,?,?,?,?,?,?)",(Category,Resume,Clean_Resume,Qualification,City,Skills,Phone_Number,Email_id))
+            postgres_insert_query = """ INSERT INTO user_data (Category,Resume,Clean_Resume,Qualification,City,Skills,Phone_Number,Email_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
+            record_to_insert = (Category,Resume,Clean_Resume,Qualification,City,Skills,Phone_Number,Email_id)
+            cursor.execute(postgres_insert_query, record_to_insert)
             conn.commit()
             conn.close()
             st.success('Resume uploaded successfully.')
